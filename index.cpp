3f4e606064850a4b7c8eea41d05f4b1220041747
Sharadamani K N <sharadamanikn@gmail.com>
2025-03-06 16:32:57 +0530

    Added index.cpp file to sub branch

---
+++ index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  
+using namespace std;  
+
+int main() {  
+    cout << "Hello, World!" << endl;  
+
+    int a = 5, b = 10;  
+    int sum = a + b;  
+
+    cout << "Sum of " << a << " and " << b << " is: " << sum << endl;  
+
+    return 0;  
+}
